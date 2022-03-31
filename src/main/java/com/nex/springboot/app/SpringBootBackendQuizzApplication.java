0945a9d1613c4b697e462439a64983f5f6749764
Juan Melo Pérez <juanmlstm@gmail.com>
2022-03-31 23:59:04 +0100

    First commit

---
+++ src/main/java/com/nex/springboot/app/SpringBootBackendQuizzApplication.java
@@ -0,0 +1,13 @@
+package com.nex.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendQuizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nex/springboot/app/SpringBootBackendQuizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nex.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendQuizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
