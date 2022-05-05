cc46cb1447f1698f33a9a3ae00b76f2f95ba2884
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-05 13:05:58 +0100

    Added new methods to save categories and languages

--- src/main/java/com/nex/springboot/backend/quizz/controllers/CategoryRestService.java
+++ src/main/java/com/nex/springboot/backend/quizz/controllers/CategoryRestService.java
@@ -1,10 +1,21 @@
 package com.nex.springboot.backend.quizz.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +34,26 @@ public class CategoryRestService {
 	public List<Category> index() {
 		return categoryService.findAll();
 	}
-	
+
+	@PostMapping("/categories")
+	public ResponseEntity<?> create(@Valid @RequestBody Category category, BindingResult result) {
+		Category newCategory = null;
+		Map<String, Object> response = new HashMap<>();
+		if (result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "Field '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			response.put("errors", errors);
+		}
+		try {
+			newCategory = categoryService.save(category);
+		} catch (DataAccessException e) {
+			response.put("message", "Error insert new category");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("message", "New category created");
+		response.put("category", newCategory);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+
 }
--- src/main/java/com/nex/springboot/backend/quizz/controllers/LanguageRestController.java
+++ src/main/java/com/nex/springboot/backend/quizz/controllers/LanguageRestController.java
@@ -1,10 +1,21 @@
 package com.nex.springboot.backend.quizz.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +33,27 @@ public class LanguageRestController {
 	@GetMapping("/languages")
 	public List<Language> index() {
 		return languageService.findAll();
+	}
+	
+	@PostMapping("/languages")
+	public ResponseEntity<?> create(@Valid @RequestBody Language language, BindingResult result) {
+		Language newLanguage = null;
+		Map<String, Object> response = new HashMap<>();
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "Field '" + err.getField() + "' " + err.getDefaultMessage())
+					.collect(Collectors.toList());
+			response.put("errors", errors);
+		}
+		try {
+			newLanguage = languageService.save(language);
+		} catch (DataAccessException e) {
+			response.put("message", "Error insert new language");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("message", "New language created");
+		response.put("language", newLanguage);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 
 }
